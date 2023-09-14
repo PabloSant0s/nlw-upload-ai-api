f2ed80c3042c2d95d9b62188179750f81c0a56ce
Pablo Santos <pablorasantos.dev@gmail.com>
2023-09-14 10:23:11 -0300

    [feat]Primeiro Commit

---
+++ src/server.ts
@@ -0,0 +1,18 @@
+import { prisma } from './lib/prisma'
+import { fastify } from 'fastify'
+
+const app = fastify()
+
+app.get('/prompts', async () => {
+  const prompts = await prisma.prompt.findMany()
+
+  return prompts
+})
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log('HTTP Server running!')
+  })
